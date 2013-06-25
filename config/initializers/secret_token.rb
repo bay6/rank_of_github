ce7c39c7c5bd359cdb369d0565adcee1b3d2349b
vincent178 <vincent.007.cn@gmail.com>
2013-06-25 22:30:02 +0800

    init app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RankOfGithub::Application.config.secret_key_base = '098570f77a853fafadfe18ef18b250c50b90e5142e12d6917219cd58ebc04c1a39c2f9575bc34ddc55bbfe1d9e67c0ce29659845afd5f16372d26def8095f61c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RankOfGithub::Application.config.session_store :cookie_store, key: '_rank_of_github_session'
